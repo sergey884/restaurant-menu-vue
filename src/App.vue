ac7a590d2c37671e4500ad743aa07e08250ef0f5
Sergey Avanesyan <ser.avanesyan@gmail.com>
2018-08-29 20:38:44 +0300

    [master] Create initial app.

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="app">
+        {{ message }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    data: function () {
+        return {
+            message: 'Hello from Vue JS',
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
